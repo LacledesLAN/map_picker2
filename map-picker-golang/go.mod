3c6717951b5db31064d902b83b3779ad84705ef2
Nathan Cowen <sk8rgui@gmail.com>
2020-01-20 18:47:28 -0600

    playing with jsonQuery package"

--- map-picker-golang/go.mod
+++ map-picker-golang/go.mod
@@ -2,4 +2,7 @@ module github.com/lacledeslan/map_picker2
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/thedevsaddam/gojsonq v2.3.0+incompatible
+)
--- map-picker-golang/pkg/controllers/games-controller.go
+++ map-picker-golang/pkg/controllers/games-controller.go
@@ -1,23 +1,45 @@
 package controllers
 
-import(
-	"encoding/json"
+import (
+	"bytes"
 	"fmt"
-	"github.com/gorilla/mux"
 	"github.com/lacledeslan/map_picker2/pkg/models"
+	"github.com/thedevsaddam/gojsonq"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 var NewGame models.Game
 
+var json = readFile()
+
+func readFile() string {
+	pwd, _ := os.Getwd()
+	//read in json file
+	filePath := filepath.Join(pwd, "data-responses", "games.json")
+
+	//convert to absolute path to reduce issues with linux docker images/OS
+	absPath, err := filepath.Abs(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//read in json file
+	data, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return string(data)
+}
 
 
 func CreateGame(w http.ResponseWriter, r *http.Request) {
-	//CreateGame := &models.Game{}
-
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(w, "You want to create a game")
 }
@@ -28,38 +50,40 @@ func DeleteGame(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetAllGames(w http.ResponseWriter, r *http.Request) {
-	pwd, _ := os.Getwd()
-	//read in json file
-	data, err := ioutil.ReadFile(pwd+ "/data-responses/games.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	dataBytes := []byte(data)
-
-	obj := models.Games{}
-
-	// unmarshall json file
-	err = json.Unmarshal(dataBytes, &obj)
-	if err != nil {
-		fmt.Println(err)
-	}
 
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, obj)
+	fmt.Fprint(w, string(json))
 }
 
 
 func GetGameByID(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	gameId := vars["gameId"]
+	gameId, _ := strconv.Atoi(vars["gameId"])
+	fmt.Println(gameId)
+	//ID, err:=strconv.ParseInt(gameId, 0 , 0)
+	//if err != nil {
+	//	fmt.Fprintf(w, "Error while parsing. '%v' is not a number", gameId)
+	//}
 
-	ID, err:=strconv.ParseInt(gameId, 0 , 0)
-	if err != nil {
-		fmt.Fprintf(w, "Error while parsing. '%v' is not a number", gameId)
-	}
+	//game := gojsonq.New().FromString(json).Where("id", "=", ID)
+
+	var b bytes.Buffer
+
+	//jq := gojsonq.New().JSONString(json)
+	//res:= jq.From("games").WhereEqual("id", gameId).Get()
+
+	gojsonq.New().JSONString(json).From("games.["+ vars["gameId"] +"]").Writer(&b)
+
+	//if err != nil {
+	//	fmt.Fprintf(w, "Error")
+	//}
+
+
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, "You are looking for a game with the ID of:  %v", ID)
+	fmt.Fprint(w, b.String())
+
+
+
 }
 
 func GetGameByName(w http.ResponseWriter, r *http.Request) {
